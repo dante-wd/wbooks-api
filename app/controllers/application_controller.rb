a889cbb668fa2ddf59f56ab87d192aaef066cecd
Dante Ventieri <dantevvp@gmail.com>
2020-05-20 17:56:57 -0300

    Corregir fallas de rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
+# frozen_string_literal: true
+
+# controller class
 class ApplicationController < ActionController::Base
 end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# record model class
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 end
