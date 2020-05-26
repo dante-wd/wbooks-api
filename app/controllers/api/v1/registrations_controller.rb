cc5adf1baee07ade973e9bfcafeb8a96f115d1e4
Dante Ventieri <dantevvp@gmail.com>
2020-05-26 14:26:03 -0300

    Corregir ofensas de rubocop

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 # Registrations controller class
-  class RegistrationsController < Devise::RegistrationsController
-    protect_from_forgery
-    respond_to :json
+class RegistrationsController < Devise::RegistrationsController
+  protect_from_forgery
+  respond_to :json
 
-    def create
-      build_resource(sign_up_params)
+  def create
+    build_resource(sign_up_params)
 
-      resource.save
-      render_resource(resource)
-    end
+    resource.save
+    render_resource(resource)
   end
+end
--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,17 +1,17 @@
 # frozen_string_literal: true
 
 # sessions controller for devise
-  class SessionsController < Devise::SessionsController
-    protect_from_forgery
-    respond_to :json
+class SessionsController < Devise::SessionsController
+  protect_from_forgery
+  respond_to :json
 
-    private
+  private
 
-    def respond_with(resource, _opts = {})
-      render json: resource
-    end
-
-    def respond_to_on_destroy
-      head :no_content
-    end
+  def respond_with(resource, _opts = {})
+    render json: resource
   end
+
+  def respond_to_on_destroy
+    head :no_content
+  end
+end
