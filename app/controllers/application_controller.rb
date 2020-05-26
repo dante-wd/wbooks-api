ea51606a7c0724c3de0ea835e02edb82dd7120e6
Dante Ventieri <dantevvp@gmail.com>
2020-05-26 14:21:19 -0300

    Agregar versionado y directorio api

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@
 
 # controller class
 class ApplicationController < ActionController::Base
+  protect_from_forgery
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def render_resource(resource)
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,20 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users,
-             path: '',
-             path_names: {
-               sign_in: 'login',
-               sign_out: 'logout',
-               registration: 'signup',
-             },
-             controllers: {
-               sessions: 'sessions',
-               registrations: 'registrations'
-             }
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope module:'api',path:'api' do
+    scope module:'v1',path:'v1' do
+      devise_for :users,
+                 path: 'users/',
+                 path_names: {
+                   sign_in: 'login',
+                   sign_out: 'logout',
+                   registration: 'signup',
+                 },
+                 controllers: {
+                   sessions: 'sessions',
+                   registrations: 'registrations'
+                 }
+      # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    end
+  end
 end
