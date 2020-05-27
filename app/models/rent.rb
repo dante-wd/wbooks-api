81a63b45de16d1d0044d34fa414da9c252f3779d
Dante Ventieri <dantevvp@gmail.com>
2020-05-27 18:33:48 -0300

    Corregir fallas de rubocop

--- app/models/rent.rb
+++ app/models/rent.rb
@@ -3,11 +3,11 @@ class Rent < ApplicationRecord
   belongs_to :book
 
   validates :from,
-  			presence: true,
-  			date: { before_or_equal_to: :to }
+            presence: true,
+            date: { before_or_equal_to: :to }
   validates :to,
-  			presence: true,
-  			date: { after_or_equal_to: :from }
+            presence: true,
+            date: { after_or_equal_to: :from }
   validates :user_id, presence: true
   validates :book_id, presence: true
 end
