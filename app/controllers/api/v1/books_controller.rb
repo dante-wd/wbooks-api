6c939b81bb6e3b8d6509a6cecd888fbcb6dc2f33
Dante Ventieri <dantevvp@gmail.com>
2020-06-01 13:17:46 -0300

    Corregir fallas de rubocop

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -5,7 +5,7 @@ module Api
         page_size = params[:page_size]
         page = [params[:page].to_i, Book.page(1).per(page_size).total_pages].min
         books = Book.page(page).per(page_size)
-        render json: books, each_serializer: Api::V1::BookSerializer, serializer: ActiveModel::Serializer::CollectionSerializer
+        render json: books, each_serializer: Api::V1::BookSerializer
       end
 
       def show
