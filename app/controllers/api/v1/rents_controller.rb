5d9140975b583732c19d2f108a79c6c748de2880
Dante Ventieri <dantevvp@gmail.com>
2020-06-02 11:31:54 -0300

    Corregir fallas de rubocop

--- app/controllers/api/v1/rents_controller.rb
+++ app/controllers/api/v1/rents_controller.rb
@@ -11,21 +11,21 @@ module Api
       end
 
       def create
-        puts "imprimiendo parametros"
-        params.each do |key, val|
-          puts key.to_s + ": " + val.to_s 
-        end
-        puts "parametros impresos"
-        puts "imprimiendo datos de sesion"
-        session.each do |key, val|
-          puts key.to_s + ": " + val.to_s 
-        end
-        puts "datos de sesion impresos"
+        # puts 'imprimiendo parametros'
+        # params.each do |key, val|
+        #  puts key.to_s + ': ' + val.to_s
+        # end
+        # puts 'parametros impresos'
+        # puts 'imprimiendo datos de sesion'
+        # session.each do |key, val|
+        #  puts key.to_s + ': ' + val.to_s
+        # end
+        # puts 'datos de sesion impresos'
         rent = Rent.new(params[:user])
         if rent.save
           render json: rent, serializer: Api::V1::RentSerializer
         else
-          render "Error?"
+          render 'Error?'
         end
       end
     end
--- app/serializers/api/v1/book_serializer.rb
+++ app/serializers/api/v1/book_serializer.rb
@@ -11,11 +11,7 @@ module Api
       attribute :image, key: :image_url
 
       def available
-        available = if object.rent[:to] > Date.today
-                      false
-                    else
-                      true
-                    end
+        available = object.rent[:to] <= Time.zone.today
         available
       end
     end
